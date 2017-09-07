1e56a727dcfd22fb30cfc017773152f256a2b00a
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-07 03:53:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -16,3 +16,13 @@ def position_taken?(board, index)
 end
 
 #apparently this method wont properly execute
+'def position_taken?(board, index)
+
+  if board[index] == " " || ""
+    return false
+  elsif board[index] == "X" || "O"
+    return true
+  end
+
+end
+'
