328e4cdbe37d653f4e65ea62230a2ae2b00da789
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-07 03:22:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,8 @@ def position_taken?(board, index)
 
   if board[index] == " " || ""
     return false
-  elsif board[index] == "X" || "O"
+  end
+  if board[index] == "X" || "O"
     return true
   end
 
